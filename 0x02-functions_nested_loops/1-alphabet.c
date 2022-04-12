2d6752af19d3e8073efcaf768cd0e9244056cab0
AGwillmkawy1 <engplaubert@gmail.com>
2022-04-12 09:24:08 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,13 @@
 
 /**
  * print_alphabet - print the alphabet in a lowercase followed by a new line.
- * Return:return nothing
  */
 void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z' ; letter++)
-{
-	_putchar(letter);
-	_putchar('\n');
-}	
-	return;
+
+_putchar(letter);
+	
+_putchar('\n');
 }	
